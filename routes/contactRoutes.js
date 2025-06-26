51026051dbcb0e2572371f3ef4d65f2edae50fc1
harshil62-cell <harshil2461@gmail.com>
2025-06-26 16:05:39 +0530

    integrated swagger docs

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const {getContacts,getContact,createContact,updateContact,deleteContact}=require('../controllers/contactController');
 const validateToken = require('../middleware/validateTokenHandler');
-//will be applicable on all routes of Contact
+
 router.use(validateToken);
 
 router.get('/',getContacts);
--- server.js
+++ server.js
@@ -1,22 +1,32 @@
-const express=require('express');
-const dotenv=require('dotenv').config();
-const contactRoutes=require('./routes/contactRoutes.js');
-const userRoutes=require('./routes/userRoutes.js');
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const dotenv = require('dotenv').config();
+const contactRoutes = require('./routes/contactRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
 const { errorHandler } = require('./middleware/errorHandler.js');
 const { connectDb } = require('./config/dbConnection.js');
 
 connectDb();
-const app=express();
+const app = express();
+const port = process.env.PORT || 5000;
 
-const port=process.env.PORT;
+// Load Swagger YAML file
+const swaggerDocument = YAML.load('./swagger.yaml');
 
+// Serve Swagger docs from YAML
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Middleware
 app.use(express.json());
-//Routes
-app.use('/api/contacts',contactRoutes);
-app.use('/api/users',userRoutes);
-//Middleware for handling error
+
+// API Routes
+app.use('/api/contacts', contactRoutes);
+app.use('/api/users', userRoutes);
+
+// Error handler
 app.use(errorHandler);
 
-app.listen(port,()=>{
-    console.log(`server started on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
